db20ffd5b1dc7dcabbf701a64edfe7be2b469f8b
Azat5 <68495387+Azat5@users.noreply.github.com>
2020-11-17 08:09:23 +0300

    first commit

---
+++ Assets/Scripts/ClickToDifference.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ClickToDifference : MonoBehaviour
+{
+    public static byte numberOfDifferences, numberOfErrors;
+    public Text differencesFound, errorsFound;
+    public GameObject ok;
+    public AudioSource correct;
+
+    private void Start()
+    {
+        numberOfDifferences = 0;
+        numberOfErrors = 0;
+    }
+
+    private void Update()
+    {
+        differencesFound.text = string.Empty + numberOfDifferences;
+        errorsFound.text = string.Empty + numberOfErrors;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider.gameObject.tag == "differences")
+            {
+                correct.Play();
+                numberOfDifferences++;
+                hit.collider.gameObject.tag = "Untagged";
+                GameObject.Find(hit.collider.gameObject.name).tag = "Untagged";
+                Instantiate(ok, hit.point, Quaternion.identity);
+            }
+        }
+
+        if (numberOfDifferences == 5 && !correct.isPlaying)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            numberOfDifferences = 0;
+            numberOfErrors = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/ClickToImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToImage : MonoBehaviour
+{
+    public GameObject error;
+    public AudioSource incorrect;
+    private GameObject currentGameObject;
+    private Stack<GameObject> stack = new Stack<GameObject>();
+
+    private void Update()
+    {
+        stack.Push(currentGameObject);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(stack.Pop());
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider.gameObject == gameObject)
+            {
+                incorrect.Play();
+                ClickToDifference.numberOfErrors++;
+                currentGameObject = Instantiate(error, hit.point, Quaternion.identity) as GameObject;
+            }
+        }
+    }
+}
